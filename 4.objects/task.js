910408cafbb190c2d6238c34e1e370941638a5e9
Sergey fe-64 <semenenko.sse@mail.ru>
2023-11-08 23:14:53 +0300

    Initial commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+  if (this.marks) {
+    this.marks.push(...marks);
+  }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  } else {
+	return this.marks.reduce((marks, item) => marks + item / this.marks.length, 0);
+  }
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
 }
